349177889b9a618a7fd857a06c5a33d900115d14
Sparky yy <nicole.barnettforste@live.nmit.ac.nz>
2021-03-02 10:55:25 +1300

    reeeeeeeeeeeee

---
+++ src/Index.js
@@ -0,0 +1,12 @@
+Definition
+
+Pair programming consists of two programmers sharing a single workstation (one screen, keyboard and mouse among the pair). The programmer at the keyboard is usually called the “driver”, the other, also actively involved in the programming task but focusing more on overall direction is the “navigator”; it is expected that the programmers swap roles every few minutes or so.
+Also Known As
+
+More simply “pairing”; the phrases “paired programming” and “programming in pairs” are also used, less frequently.
+Common Pitfalls
+
+    both programmers must be actively engaging with the task throughout a paired session, otherwise no benefit can be expected
+    a simplistic but often raised objection is that pairing “doubles costs”; that is a misconception based on equating programming with typing – however, one should be aware that this is the worst-case outcome of poorly applied pairing
+    at least the driver, and possibly both programmers, are expected to keep up a running commentary; pair programming is also “programming out loud” – if the driver is silent, the navigator should intervene
+    pair programming cannot be fruitfully forced upon people, especially if relationship issues, including the most mundane (such as personal hygiene), are getting in the way; solve these first!
